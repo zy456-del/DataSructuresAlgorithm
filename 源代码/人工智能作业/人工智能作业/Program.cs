4417387bcdfb4d9f6aba833c34898a7373aad057
admin <admin@LAPTOP-J2FBD85M>
2019-12-20 13:21:29 +0800

    1

---
+++ 源代码/人工智能作业/人工智能作业/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 人工智能作业
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("方程的解为：{0}");
+            Console.Read();
+        }
+       
+    }
+}
